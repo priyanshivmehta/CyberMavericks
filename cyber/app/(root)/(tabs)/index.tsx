b2d259b970027cffe76db7cdf6540f277bc4a0ac
priyanshivmehta <pvmehta112@gmail.com>
2025-01-29 15:44:25 +0530

    Initial setUp

---
+++ cyber/app/(root)/(tabs)/index.tsx
@@ -0,0 +1,18 @@
+import {Text, View} from "react-native";
+import {Link} from "expo-router";
+export default function Index(){
+  return (
+    <View
+      style={{
+        flex:1,
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}
+    >
+      <Link href="/sign-in">Sign in</Link>
+      <Link href="/explore">Explore</Link>
+      <Link href="/profile">Profile</Link>
+      <Link href="/properties/[id]">Property</Link>
+    </View>
+  );
+}
---
+++ cyber/app/sign-in.tsx
@@ -0,0 +1,12 @@
+import { View, Text} from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const SignIn = () => {
+    return (
+        <SafeAreaView>
+            <Text>Sign In</Text>
+        </SafeAreaView>
+    )
+}
+export default SignIn
